2a0f27184661b13d98864f20659364a2f9dc152d
nhantt <nhanttn92@gmail.com>
2018-01-20 00:17:31 +0700

    learn native script

---
+++ platforms/ios/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/HelloWorld/HelloWorld-Prefix.pch \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/main.m \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/TNSDebugging.h \
+  /Users/nhantt/Desktop/HelloWorld/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
